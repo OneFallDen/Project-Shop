932c65b57a16a3c86c6f48871694880012d042c6
OneFallDen <81654497+OneFallDen@users.noreply.github.com>
2022-06-26 14:00:41 +0300

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    $_SESSION['au'] = false;
-    if($_SESSION['au'] == false){
-        echo "Logout succesful";
-    }
-?>
-<a href="auth.html">Auth </a>
-<a href="reg.html">Reg</a>
